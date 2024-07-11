c6a8cdb70b829fd4e3a0b4c060e1bd74753f4d14
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-07-11 14:52:31 -0300

    mysql config

--- db/database.js
+++ db/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'mydatabase'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 })
 
 connection.connect((error) => {
