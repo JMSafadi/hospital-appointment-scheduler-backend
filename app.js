c1854d1be5298066c9668389f8836705469cd22b
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-07-16 22:21:23 -0300

    add routers

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const express = require('express')
 const cors = require('cors')
-const pool = require('./db/database')
-
 
 const port = 3000
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-
-// Routes
+// Routers
+const initializeDBRouter = require('./routes/initializeDB.router')
 const patientsRouter = require('./routes/patients.router')
 const doctorsRouter = require('./routes/doctors.router')
 const appointmentsRouter = require('./routes/appointments.router')
+const hospitalsRouter = require('./routes/hospitals.router')
+const specializationsRouter = require('./routes/specializations.router')
 
+app.use('/api', initializeDBRouter)
 app.use('/api/patients', patientsRouter)
 app.use('/api/doctors', doctorsRouter)
 app.use('/api/appointments', appointmentsRouter)
-
-app.get('/api', (req, res) => {
-  res.json('Welcome to app backend!')
-})
-
+app.use('/api/hospitals', hospitalsRouter)
+app.use('/api/specializations', specializationsRouter)
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Server running in PORT: ${port}`)
