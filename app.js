bc09eecb1ef8919db93390431a2f494e284bc3c1
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-07-22 14:41:52 -0300

    db pool config

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 const express = require('express')
 const cors = require('cors')
-const connectWithRetry = require('./db/database')
+const connectDatabase = require('./db/database')
 
 const port = 3000
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-// Routers
-const initializeDBRouter = require('./routes/initializeDB/router')
-const patientsRouter = require('./routes/patients/router')
-const doctorsRouter = require('./routes/doctors/router')
-const appointmentsRouter = require('./routes/appointments/router')
-const hospitalsRouter = require('./routes/hospitals/router')
-const specializationsRouter = require('./routes/specializations/router')
+connectDatabase()
+.then(pool => {
+  // Set db pool to all app context
+  app.set('pool', pool)
 
-connectWithRetry()
-  .then(pool => {
-    // Set db pool to all app context
-    app.set('pool', pool)
+  // Routers
+  const initializeDBRouter = require('./routes/initializeDB/router')
+  const patientsRouter = require('./routes/patients/router')
+  const doctorsRouter = require('./routes/doctors/router')
+  const appointmentsRouter = require('./routes/appointments/router')
+  const hospitalsRouter = require('./routes/hospitals/router')
+  const specializationsRouter = require('./routes/specializations/router')
 
-    // Config routes
-    app.use('/api/v1/', initializeDBRouter)
-    app.use('/api/v1/patients', patientsRouter)
-    app.use('/api/v1/doctors', doctorsRouter)
-    app.use('/api/v1/appointments', appointmentsRouter)
-    app.use('/api/v1/hospitals', hospitalsRouter)
-    app.use('/api/v1/specializations', specializationsRouter)
-
-    // Initialize server
-    app.listen(port, () => {
-      console.log(`Server running in PORT: ${port}`)
-    })
+  // Config routes
+  app.use('/api/v1/', initializeDBRouter)
+  app.use('/api/v1/patients', patientsRouter)
+  app.use('/api/v1/doctors', doctorsRouter)
+  app.use('/api/v1/appointments', appointmentsRouter)
+  app.use('/api/v1/hospitals', hospitalsRouter)
+  app.use('/api/v1/specializations', specializationsRouter)
+  
+  // Initialize server
+  app.listen(port, () => {
+    console.log(`Server running in PORT: ${port}`)
   })
-  .catch(err => {
-    console.error('Failted to connect to database: ', err)
-    process.exit(1)
-  })
+  
+})
+.catch(err => console.error('Failed to initialize database connection: ', err))
--- db/database.js
+++ db/database.js
@@ -1,31 +1,21 @@
-const { Pool } = require('pg')
 require('dotenv').config()
+const { Pool } = require('pg')
 
-  const connectWithRetry = async (retries = 5, delay = 5000) => {
-    while (retries > 0) {
-      try {
-        const pool = new Pool({
-          host: process.env.DB_HOST,
-          user: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
-          port: process.env.DB_PORT || 5432
-        })
-        await pool.connect()
-        console.log('Connected to database successfully')
-        return pool
-      } catch (err) {
-        console.error('Database connection error: ', err)
-        retries -= 1
-        if (retries > 0) {
-          console.log(`Retrying in ${delay / 1000} seconds...`)
-          await new Promise(res => setTimeout(res, delay))
-        } else {
-          console.error('Failed to connect to database after multiple attempts')
-          process.exit(1)
-        }
-      }
-    }
+const connectDatabase = async () => {
+  const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 5432
+  })
+  try {
+    await pool.connect()
+    console.log('Connected to database successfully')
+    return pool
+  } catch (err) {
+    console.error('Database connection error: ', err)
   }
+}
 
-module.exports = connectWithRetry
+module.exports = connectDatabase
