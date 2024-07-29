90cc58c9192946cb8876afdcd4cecccc8154420e
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-07-29 15:29:29 -0300

    [add] is_available field

--- routes/availabilities/controller.js
+++ routes/availabilities/controller.js
@@ -5,7 +5,7 @@ const getAvailabilities = async (req, res) => {
   try {
     const pool = req.app.get('pool')
     const client = await pool.connect()
-    const results = await client.query(queries.getAppointments)
+    const results = await client.query(queries.getAvailabilities)
     client.release()
     res.status(200).json(results.rows)
   } catch (err) {
@@ -19,7 +19,10 @@ const getAvailabilityById = async (req, res) => {
   const id = parseInt(req.params.id)
   try {
     const client = await pool.connect()
-    const results = await client.query(queries.getAppointmentById, [id])
+    const results = await client.query(queries.getAvailabilityById, [id])
+    if (!results.rows.length) {
+      return res.status(404).json({ message: 'ID availability not found.'} )
+    }
     client.release()
     res.status(200).json(results.rows)
   } catch (err) {
--- routes/availabilities/queries.js
+++ routes/availabilities/queries.js
@@ -1,7 +1,24 @@
-const getAppointments = 'SELECT * FROM Availabilities;'
-const getAppointmentById = 'SELECT * FROM Availabilities WHERE id = $1,'
+const getAvailabilities = `
+SELECT
+  availabilities.id,
+  doctors.name AS doctor,
+  availabilities.availability_time,
+  availabilities.is_available
+FROM Availabilities
+JOIN Doctors ON availabilities.doctor_id = doctors.id;
+`
+const getAvailabilityById = `
+SELECT
+  availabilities.id,
+  doctors.name AS doctor,
+  availabilities.availability_time,
+  availabilities.is_available
+FROM Availabilities 
+JOIN Doctors ON availabilities.doctor_id = doctors.id
+WHERE id = $1;
+`
 
 module.exports = {
-  getAppointments,
-  getAppointmentById
+  getAvailabilities,
+  getAvailabilityById
 }
