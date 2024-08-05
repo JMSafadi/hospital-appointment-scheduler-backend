0b2fed196a58995a8adf4679451c493ac892b1d3
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-08-05 16:19:55 -0300

    delete logs

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -3,10 +3,8 @@ const jwt = require('../lib/jwt')
 const authenticateToken = (req, res , next) => {
   const token = req.header('x-auth-token')
   if (!token) return res.sendStatus(401)
-  console.log(token)
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET)
-    console.log('user: ', user)
      req.user = user
      next()
   } catch(err) {
