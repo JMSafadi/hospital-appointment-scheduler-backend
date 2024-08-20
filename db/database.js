ce60512e138f37d73c79d306e462d444607a32c6
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-08-19 21:29:41 -0300

    database connection

--- db/database.js
+++ db/database.js
@@ -1,21 +1,68 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
+let devPool
+let testPool
+
 const connectDatabase = async () => {
-  const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5432
-  })
-  try {
-    await pool.connect()
-    console.log('Connected to database successfully')
-    return pool
-  } catch (err) {
-    console.error('Database connection error: ', err)
+  if (process.env.NODE_ENV === 'test') {
+    try {
+      if (!testPool) {
+        testPool = new Pool({
+          host: process.env.TEST_DB_HOST,
+          user: process.env.TEST_DB_USER,
+          password: process.env.TEST_DB_PASS,
+          database: process.env.TEST_DB_NAME,
+          port: process.env.TEST_DB_PORT
+        })
+      }
+      return testPool
+    } catch (err) {
+      console.error('Error connecting to test database:', err)
+      throw err
+    }
+  } else {
+    try {
+      if (!devPool) {
+        devPool = new Pool({
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASS,
+          database: process.env.DB_NAME,
+          port: process.env.DB_PORT
+        })
+      }
+      await devPool.connect()
+      console.log('Development pool connected:', devPool)
+      return devPool
+    } catch (err) {
+      console.error('Error connecting to database:', err)
+      throw err
+    }
   }
 }
 
-module.exports = connectDatabase
+const closeDatabase = async () => {
+  if (process.env.NODE_ENV === 'test' && testPool) {
+    try {
+      await testPool.end()
+      testPool = null
+      console.log('Test database pool closed.')
+    } catch (err) {
+      console.error('Error closing test database pool:', err)
+    }
+  } else if (devPool) {
+    try {
+      await devPool.end()
+      devPool = null
+      console.log('Development database pool closed.')
+    } catch (err) {
+      console.error('Error closing development database pool:', err)
+    }
+  }
+}
+
+module.exports = {
+  connectDatabase,
+  closeDatabase
+}
