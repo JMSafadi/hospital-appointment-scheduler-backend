dde0ed79908ce96aa63d3d36933528e52bb84a36
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-08-21 13:11:26 -0300

    beforeAll init DB fn

--- jest.setup.js
+++ jest.setup.js
@@ -1,54 +1,22 @@
-const { connectDatabase, closeDatabase } = require('./db/database')
-const request = require('supertest')
-const app = require('./app')
-// const { createTestUserAndGetToken } = require('./utils/authTest')
+const { initializeDatabase } = require('./utils/initializeDbTest')
 
-let client
+let isDatabaseInitialized = false
 
 beforeAll(async () => {
-  console.log('starting tests')
-  const testPool = await connectDatabase()
-  app.set('testPool', testPool)
-  client = await testPool.connect()
-  app.set('dbClient', client)
-  
-  const responseDelete = await request(app).delete('/api/v1')
-  console.log('database cleanup before test: ', responseDelete.body)
-
-  // const result = await client.query('SELECT * from Doctors')
-  // console.log('should be empty: ', result.rows)
-
-  const initializeResponse = await request(app)
-    .post('/api/v1/')
-    .send()
-  if (initializeResponse.statusCode === 200) {
-    console.log('Tables and data initialize for tests')
+  if (!isDatabaseInitialized) {
+    await initializeDatabase()
+    isDatabaseInitialized = true
   }
-    
-  const userResponse = await request(app)
-    .post('/api/v1/signin')
-    .send({
-      name: 'Test User', 
-      email: 'testuser@example.com', 
-      password: 'password123' 
-    })
-
-    const loginResponse = await request(app)
-    .post('/api/v1/login')
-    .send({ email: 'testuser@example.com', password: 'password123' })
-
-  const token = loginResponse.body.jwt
-
-  app.set('testUser', userResponse.body)
-  app.set('testToken', token)
-
 })
 
+
 // afterAll(async () => {
-//   const testPool = app.get('testPool')
-//   let client = app.get('dbClient')
-//   client.release()
+//   // let client = app.get('dbClient')
+//   // client.release()
 //   if (!testPool.ended) {
 //     await testPool.end()
+//     console.log('test pool ended')
+//   } else {
+//     console.log('test pool not closed')
 //   }
 // })
---
+++ utils/initializeDbTest.js
@@ -0,0 +1,47 @@
+const request = require('supertest')
+const app = require('../app')
+const { connectDatabase } = require('../db/database')
+
+let client
+let testPool
+
+async function initializeDatabase() {
+  testPool = await connectDatabase()
+  app.set('testPool', testPool)
+  client = await testPool.connect()
+  app.set('dbClient', client)
+  
+  const responseDelete = await request(app)
+    .delete('/api/v1')
+  console.log('Database cleanup before tests: ', responseDelete.body)
+
+  const initializeResponse = await request(app)
+    .post('/api/v1/')
+    .send()
+  if (initializeResponse.statusCode === 200) {
+    console.log('Tables and data initialized for tests')
+  }
+
+  const userResponse = await request(app)
+    .post('/api/v1/signin')
+    .send({
+      name: 'Test User',
+      email: 'testuser@example.com',
+      password: 'password123',
+    })
+
+  const loginResponse = await request(app)
+    .post('/api/v1/login')
+    .send({ email: 'testuser@example.com', password: 'password123' })
+
+  const token = loginResponse.body.jwt
+
+  app.set('testUser', userResponse.body)
+  app.set('testToken', token)
+
+  console.log('Database initialization complete')
+}
+
+module.exports = {
+  initializeDatabase
+}
