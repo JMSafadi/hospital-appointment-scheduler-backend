f9dfa34aec4a255beddcaecc5fb7b8cd7ceceb8e
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-08-22 14:33:28 -0300

    delte logs

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,6 @@
 const { initializeDatabaseAndUser } = require('./utils/initializeDbTest')
 
 beforeAll(async () => {
-  console.log('Initializing database and user for tests.')
   await initializeDatabaseAndUser()
 })
 
