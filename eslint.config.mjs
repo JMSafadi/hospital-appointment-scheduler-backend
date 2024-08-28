01d1477e083a813c5827dd57565ad9eb684a2ccb
Julian Safadi <julianmatiassafadi@hotmail.com>
2024-08-28 12:45:34 -0300

    ESLint rules

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+export default [
+  {
+    'rules': {
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+
+      'eqeqeq': 'error',
+      'no-console': 'warn',
+      'no-unused-vars': ['error'],
+
+      'prefer-const': 'error',
+      'no-var': 'error',
+
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always']
+    }
+  }
+]
